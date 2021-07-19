0fd57a18d76a906bcbb68d707633ae1a45b5ace4
Jorge Conce <jconcehernandez@gmail.com>
2021-07-19 18:26:38 -0400

    primer commit

---
+++ phone_number_information .py
@@ -0,0 +1,11 @@
+
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
+
+phone_number = phonenumbers.parse("Enter mobile number")
+
+print(geocoder.description_for_number(phone_number, 'en'))
+
+print(carrier.name_for_number(phone_number, 'en'))
+
+print(timezone.time_zones_for_number(phone_number))
