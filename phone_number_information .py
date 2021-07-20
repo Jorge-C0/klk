31a069541918a6f4ff2097b30eceeb49aeb74e14
Jorge Conce <jconcehernandez@gmail.com>
2021-07-19 21:36:06 -0400

    codigo de today_date

--- phone_number_information .py
+++ phone_number_information .py
@@ -9,3 +9,9 @@ print(geocoder.description_for_number(phone_number, 'en'))
 print(carrier.name_for_number(phone_number, 'en'))
 
 print(timezone.time_zones_for_number(phone_number))
+
+print("Final")
+print("Final1")
+print("Final2")
+print("Final3")
+print("Final54")
